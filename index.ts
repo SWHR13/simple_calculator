de9049b2448bb70d94a2ba8c9dc42cb191fe51b4
SWHR13 <walimr48@gmail.com>
2024-05-28 21:46:04 +0500

    tweak the text and added console at the end

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 
 import inquirer from "inquirer";
 const answer = await inquirer.prompt([
-    { message: "Enter first number:", type: "number", name: "firstNumber" },
-    { message: "Enter second number:", type: "number", name: "secondNumber" },
+    { message: "Enter your first number:", type: "number", name: "firstNumber" },
+    { message: "Enter your second number:", type: "number", name: "secondNumber" },
     {
         message: "Select one of the operators to perform action",
         type: "list",
@@ -27,6 +27,8 @@ else if (answer.operator == "Division") {
 else {
     console.log("Please enter valid operator");
 }
+
+console.log("THE END!");
 
 
 
